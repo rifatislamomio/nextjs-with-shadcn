3bd34ba49ee7b7aa3acb8404c5374ec0fdc77cbd
Rifat <rifat.islam173@northsouth.edu>
2024-10-29 20:52:47 +0600

    added env

--- app/api/axios.tsx
+++ app/api/axios.tsx
@@ -4,6 +4,6 @@ export const axiosClient = axios.create({
   headers: {
     "Content-Type": "application/json;charset=utf-8"
   },
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
   withCredentials: false
 });
